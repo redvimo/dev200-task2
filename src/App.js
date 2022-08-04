b22ee1264983de2f0094ef91f10f90478365b8e2
Vimalan Reddy <redvimo@gmail.com>
2022-08-05 00:28:17 +0200

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import './App.css';
 
 const currentDate = new Date('2016-05-18T11:27:00');
 
+const availabilityData = [
+  {
+    "Date": "2016-05-18",
+    "HoursAvailable": [9, 10, 11, 12, 13, 14, 17]
+  },
+  {
+    "Date": "2016-05-19",
+    "HoursAvailable": [9, 10, 11, 12, 13, 14, 15, 16, 17]
+  },
+  {
+    "Date": "2016-05-20",
+    "HoursAvailable": [9, 10, 14, 15, 16, 17]
+  },
+  {
+    "Date": "2016-05-21",
+    "HoursAvailable": [9, 10, 11, 12, 13]
+  },
+  {
+    "Date": "2016-05-23",
+    "HoursAvailable": [13, 14, 15, 16]
+  },
+  {
+    "Date": "2016-05-24",
+    "HoursAvailable": [11, 12, 15, 16, 17]
+  }
+];
+
+function checkSlotAvailability(time, jobLength, date, availability) {
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>{currentDate.toString()}</p>
+        
+        
         <a
           className="App-link"
           href="https://reactjs.org"
