cdd37dfed36b475b3aa7e9d9b1e78a1c47340451
Vimalan Reddy <redvimo@gmail.com>
2022-08-05 13:28:10 +0200

    Changes to be committed: 	modified:   src/App.js 	modified:   src/booking.js

--- src/App.js
+++ src/App.js
@@ -1,54 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {currentDate, availabilityStatus} from './booking.js';
 
-const currentDate = new Date('2016-05-18T11:27:00');
 
-const availabilityData = [
-  {
-    "Date": "2016-05-18",
-    "HoursAvailable": [9, 10, 11, 12, 13, 14, 17]
-  },
-  {
-    "Date": "2016-05-19",
-    "HoursAvailable": [9, 10, 11, 12, 13, 14, 15, 16, 17]
-  },
-  {
-    "Date": "2016-05-20",
-    "HoursAvailable": [9, 10, 14, 15, 16, 17]
-  },
-  {
-    "Date": "2016-05-21",
-    "HoursAvailable": [9, 10, 11, 12, 13]
-  },
-  {
-    "Date": "2016-05-23",
-    "HoursAvailable": [13, 14, 15, 16]
-  },
-  {
-    "Date": "2016-05-24",
-    "HoursAvailable": [11, 12, 15, 16, 17]
-  }
-];
-
-function checkSlotAvailability(time, jobLength, date, availability) {
-
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{currentDate.toDateString()}</p>
+        <p>{availabilityStatus.toString()}</p>
       </header>
     </div>
   );
--- src/booking.js
+++ src/booking.js
@@ -1,11 +1,51 @@
-const d = new Date("2016-05-18T11:27:00");
-const cdn = 'http://example.com/movies.jsonhttps://techki-bucket-cos-standard-13b.s3.eu-de.cloud-object-storage.appdomain.cloud/availability.json';
+//booking.js
 
-console.log(d);
+export let time = 9;
+export let jobLength = 1;
+export let currentDate = new Date('2016-05-18T11:27:00');
 
-function checkSlotAvailability() {
-    fetch(cdn)
-    .then((response) => response.json())
-    .then((data) => {return data});
-}
+const availabilityData = [
+  {
+    "Date": "2016-05-18",
+    "HoursAvailable": [9, 10, 11, 12, 13, 14, 17]
+  },
+  {
+    "Date": "2016-05-19",
+    "HoursAvailable": [9, 10, 11, 12, 13, 14, 15, 16, 17]
+  },
+  {
+    "Date": "2016-05-20",
+    "HoursAvailable": [9, 10, 14, 15, 16, 17]
+  },
+  {
+    "Date": "2016-05-21",
+    "HoursAvailable": [9, 10, 11, 12, 13]
+  },
+  {
+    "Date": "2016-05-23",
+    "HoursAvailable": [13, 14, 15, 16]
+  },
+  {
+    "Date": "2016-05-24",
+    "HoursAvailable": [11, 12, 15, 16, 17]
+  }
+];
+
+function checkSlotAvailability(time, jobLength, date, availability) {
+    return "FULL";
+}    
+
+export const availabilityStatus = availabilityData.map((value, index, array) => {
+  let year = new Date(value.Date).getFullYear();
+  let month = new Date(value.Date).getMonth();
+  let date = new Date(value.Date).getDate();
+  let status = "FULL";
+
+  if (year === currentDate.getFullYear()
+    && month === currentDate.getMonth()
+    && date === currentDate.getDate()) {
+      status = checkSlotAvailability(time, jobLength, value.Date, value.HoursAvailable);
+    }
+  return status;
+});
 
