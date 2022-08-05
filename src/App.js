00dc52bc45054787243803b427f1b65e9c16d6fc
Vimalan Reddy <redvimo@gmail.com>
2022-08-05 16:17:50 +0200

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Availability Calendar</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>{currentDate.toDateString()}</p>
         <p>{availabilityStatus.toString()}</p>
